7540c9f24faa0477f3ca7a06042de15c1bb7e07a
HasanAttar <hasanattar40@gmail.com>
2024-07-24 11:01:08 +0300

    Update put.js

--- testapi/controller/put.js
+++ testapi/controller/put.js
@@ -1,28 +1,4 @@
-// const con =require('../config/connection');
 
-// exports.put=(req,res)=>{
-//     var sql=(`update Customers set address ="baalbeck" where address ="Valley 345" `);
-    
-//     con.query(sql,function(err,result){
-        
-//         if (err) {
-//             // throw err;
-//             console.log("Table failed");
-//             return res.status(400).json({
-//                 success: false,
-//                 err
-//             })
-//         } else {
-//             console.log("Table updated");
-//             return res.status(200).json({
-//                 success:true,
-//                 result
-                
-//             })
-           
-//         }
-//     })
-// }
 
 const con = require('../config/connection');
 
